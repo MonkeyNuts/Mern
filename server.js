7146c7d646c413957fb8330ee90d6efb80e89638
Will Quinn <wrdquinn@gmail.com>
2022-04-01 14:58:13 +1300

    bug fix 1

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ require("dotenv").config()
 const path = require("path");
 
 // Step 1:
-app.use(express.static(path.resolve(__dirname, "./my-app/build")));
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 // Step 2:
 app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./my-app/build", "index.html"));
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
